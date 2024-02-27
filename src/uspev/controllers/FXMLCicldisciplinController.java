2f2e094336fc1aa55e0b03cfecd12a60778b2451
Timur <timur@bk.ru>
2024-02-27 15:38:58 +0300

    добавлен драйвер к бд и файл базы данных

--- src/uspev/controllers/FXMLCicldisciplinController.java
+++ src/uspev/controllers/FXMLCicldisciplinController.java
@@ -2,7 +2,13 @@
 package uspev.controllers;
 
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.fxml.Initializable;
 
 
@@ -11,7 +17,7 @@ public class FXMLCicldisciplinController implements Initializable {
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        
     }    
     
 }
