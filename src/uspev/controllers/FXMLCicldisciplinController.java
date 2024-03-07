482de26d58b08eb9d49594e97ed777230c969b1a
Vladimir Vasilev <vnvsul@mail.ru>
2024-03-07 23:11:15 +0300

    Промежуточная фиксация

--- src/uspev/controllers/FXMLCicldisciplinController.java
+++ src/uspev/controllers/FXMLCicldisciplinController.java
@@ -2,15 +2,12 @@
 package uspev.controllers;
 
 import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -18,11 +15,17 @@ import static javafx.collections.FXCollections.observableArrayList;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import uspev.DBManager;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TablePosition;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 import static uspev.DBManager.getConnection;
-import static uspev.DataInit.addData;
 import uspev.models.ModelCicldisciplin;
 
 
@@ -37,44 +40,79 @@ public class FXMLCicldisciplinController implements Initializable {
     private Button cklbtupdate;
     @FXML
     private Button cklbtdel;
+    @FXML
+    private static TableView<ModelCicldisciplin> cicltable;
+    @FXML
+    private TableColumn<ModelCicldisciplin, Integer> ciclnomer;
+    @FXML
+    private TableColumn<ModelCicldisciplin, String> ciclname;
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        ciclshow();
+    }   
+    public void ciclshow(){
         try {
+            ciclnomer.setCellValueFactory(new PropertyValueFactory("id"));
+            ciclname.setCellValueFactory(new PropertyValueFactory("name"));
             con=getConnection();
+           ObservableList<ModelCicldisciplin> list=getCicllist();
+           cicltable.setItems(list);
             
         } catch (SQLException ex) {
             Logger.getLogger(FXMLCicldisciplinController.class.getName()).log(Level.SEVERE, null, ex);
         }
-    }   
-
+    }
     ObservableList<ModelCicldisciplin> getCicllist() throws SQLException{
         ObservableList<ModelCicldisciplin> List=observableArrayList();
         st=con.createStatement();
-        
+        String zapros="SELECT * FROM CICLDISCIPLIN";
+        rs=st.executeQuery(zapros);
+        while (rs.next())
+        {
+            List.add(new ModelCicldisciplin(rs.getInt("ID"),rs.getString("NAME")));
+        }
         return List;
     }
+    
+   void ciclEditShow(String title) throws IOException{
+       FXMLLoader loader=new FXMLLoader();
+       loader.setLocation(getClass().getResource("/uspev/views/FXMLCikledit.fxml"));
+       loader.load();
+       Stage stage=new Stage();
+       Parent root =loader.getRoot();
+       stage.setScene(new Scene(root));
+       stage.setTitle(title);
+       stage.showAndWait();
+       ciclshow();
+             
+   }
+   
+   public static int getID(){
+       TablePosition pos = cicltable.getSelectionModel().getSelectedCells().get(0);
+        int row = pos.getRow();
+        ModelCicldisciplin item=cicltable.getItems().get(row);
+        TableColumn col = pos.getTableColumn();
+        String data = (String) col.getCellObservableValue(item).getValue();
+        System.out.println(data);
+    return 0;
+}
+    
     @FXML
-    private void onAddCicle(ActionEvent event) throws SQLException {
+    private void onAddCicle(ActionEvent event) throws SQLException, IOException {
               
-        
+        ciclEditShow("Добавление цикла дисциплин");
         
     }
 
     @FXML
-    private void onUpdateCicle(ActionEvent event) throws FileNotFoundException, SQLException {
-        System.out.println("проверка");
-       Statement statement=con.createStatement();
-       ResultSet rs=statement.executeQuery("SELECT * FROM CICLDISCIPLIN");
-       while (rs.next())
-       {
-           System.out.print(rs.getInt("ID")+"  ");
-           System.out.print(rs.getString("NAME")+"\n");
-       }
-       
+    private void onUpdateCicle(ActionEvent event) throws FileNotFoundException, SQLException, IOException {
+        
+        ciclEditShow("Редактирование цикла дисциплин");
     }
 
     @FXML
     private void onDelCicle(ActionEvent event) {
+       getID(); 
     }
     
     
---
+++ src/uspev/controllers/FXMLCikleditController.java
@@ -0,0 +1,56 @@
+
+package uspev.controllers;
+
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import static uspev.DataInit.con;
+
+
+public class FXMLCikleditController implements Initializable {
+
+    @FXML
+    private Button savebt;
+    @FXML
+    private Button canselbt;
+    @FXML
+    private TextField cicltxt;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+       
+    }    
+
+    @FXML
+    private void onSave(ActionEvent event) throws SQLException {
+     Stage st= (Stage) savebt.getScene().getWindow();
+    if ( st.getTitle()=="Добавление цикла дисциплин"){
+        String zapros="INSERT INTO CICLDISCIPLIN(NAME) VALUES(?) ";
+       Connection connection=con(); 
+       PreparedStatement prepare=connection.prepareStatement(zapros);
+        prepare.setString(1, cicltxt.getText());
+        prepare.execute();
+    }
+    else {
+        String zapros="UPDATE CICLDISCIPLIN SET NAME=(?) WHERE ID=5";
+       Connection connection=con(); 
+       PreparedStatement prepare=connection.prepareStatement(zapros);
+        prepare.setString(1, cicltxt.getText());
+        prepare.execute();
+    }
+    }
+
+    @FXML
+    private void onCansel(ActionEvent event) {
+        savebt.getScene().getWindow().hide();
+    }
+    
+}
